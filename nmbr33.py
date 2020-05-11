059e08e43d111c20bdf983fa35fb574376147cf7
szeemi00 <63341078+szeemi00@users.noreply.github.com>
2020-05-11 23:31:53 +0200

    " "

--- nmbr33.py
+++ nmbr33.py
@@ -3,22 +3,22 @@ def mailCim (mail):
         return "Nem valid"
     mail = mail.split("@")
     for i in mail[0]:
-        if i=="_" or i=="," or i=="?" or i=="!" or i=="*" or i==":" or i==";":
+        if i == "_" or i == "," or i == "?" or i == "!" or i == "*" or i == ":" or i == ";":
             return "Nem valid"
     domain = mail[1].split(".")
     db=0
     for j in domain[0]:
-        if db==0:
-            if j.isupper()==True:
+        if db == 0:
+            if j.isupper() == True:
                 return "Nem valid"
-            db+=1
-        if j=="_" or j=="," or j=="?" or j=="!" or j=="*" or j==":" or j==";":
+            db += 1
+        if j == "_" or j == "," or j == "?" or j == "!" or j == "*" or j == ":" or j == ";":
             return "Nem valid"
     ls=["com","hu"]
     for l in ls:
-        if domain[len(domain)-1]==l:
+        if domain[len(domain)-1] == l:
            return "Valid"
     return "Nem valid"
 
-mail=input("Adja meg az e-mail címet és megállapítom, hogy valid-e: ")
+mail = input("Adja meg az e-mail címet és megállapítom, hogy valid-e: ")
 print(mailCim(mail))
--- nmbr44.py
+++ nmbr44.py
@@ -10,40 +10,40 @@ def szoras(n):
         atl_elteres_neg = 0
         pozitiv = []
         negativ = []
-        szam=input()
+        szam = input()
         ls=szam.split(" ")
         for j in (ls):
-            j=int(j)
+            j = int(j)
             if j<0:
-                neg_o+=j
+                neg_o += j
                 negativ.append(j)
             else:
-                poz_o+=j
+                poz_o += j
                 pozitiv.append(j)
         if len(pozitiv) > 1:
-            poz_atl=poz_o/len(pozitiv)
+            poz_atl = poz_o/len(pozitiv)
             for p in pozitiv:
-                tmp=(p-poz_atl)**2
-                atl_elteres_poz+=tmp
-            atl_elteres_poz=atl_elteres_poz/len(pozitiv)
+                tmp = (p-poz_atl)**2
+                atl_elteres_poz += tmp
+            atl_elteres_poz = atl_elteres_poz/len(pozitiv)
             atl_elteres_poz = math.sqrt(atl_elteres_poz)
-            atl_elteres_poz=f"{atl_elteres_poz:0.3}"
+            atl_elteres_poz = f"{atl_elteres_poz:0.3}"
         else:
-            atl_elteres_poz="nem számolható"
-        if len(negativ)>1:
-            neg_atl=neg_o/len(negativ)
+            atl_elteres_poz = "nem számolható"
+        if len(negativ) > 1:
+            neg_atl = neg_o/len(negativ)
             for s in negativ:
-                tmp2=(s-neg_atl)**2
-                atl_elteres_neg+=tmp2
-            atl_elteres_neg=atl_elteres_neg/len(negativ)
-            atl_elteres_neg=math.sqrt(atl_elteres_neg)
+                tmp2 = (s-neg_atl)**2
+                atl_elteres_neg += tmp2
+            atl_elteres_neg = atl_elteres_neg/len(negativ)
+            atl_elteres_neg = math.sqrt(atl_elteres_neg)
             atl_elteres_neg = f"{atl_elteres_neg:0.3}"
         else:
-            atl_elteres_neg="nem számolható."
+            atl_elteres_neg = "nem számolható."
         eredmeny_poz.append(atl_elteres_poz)
         eredmeny_neg.append(atl_elteres_neg)
     for k in range(len(eredmeny_poz)):
         print("Pozitív szórás:",eredmeny_poz[k]+","+" negatív szórás:",eredmeny_neg[k])
  
-n=int(input("n= "))
+n = int(input("n= "))
 szoras(n)
